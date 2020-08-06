ab5cd9839ecee3fb71a14990dae64ec7f75f495d
eitchtee <flaips.2@gmail.com>
2020-08-06 00:24:07 -0300

    Replace TeacherList inputs for Components

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
 
 import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
+import Select from '../../components/Select';
 
 
 function TeacherList () {
@@ -10,18 +12,34 @@ function TeacherList () {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                <Select name="subject"
+                            label="Matéria"
+                            options={[
+                                {value: 'Artes', label: 'Artes'},
+                                {value: 'Matemática', label: 'Matemática'},
+                                {value: 'Biologia', label: 'Biologia'},
+                                {value: 'Ciências', label: 'Ciências'},
+                                {value: 'Física', label: 'Física'},
+                                {value: 'Quimíca', label: 'Quimíca'},
+                                {value: 'Português', label: 'Português'},
+                                {value: 'Redação', label: 'Redação'},
+                                {value: 'Geometria', label: 'Geometria'},
+                                {value: 'Sociologia', label: 'Sociologia'},
+                                {value: 'Filosofia', label: 'Filosofia'},
+                                {value: 'História', label: 'História'},
+                            ]} />
+                    <Select name="week-day"
+                            label="Dia da semana"
+                            options={[
+                                {value: '0', label: 'Domingo'},
+                                {value: '1', label: 'Segunda-feira'},
+                                {value: '2', label: 'Terça-feira'},
+                                {value: '3', label: 'Quarta-feira'},
+                                {value: '4', label: 'Quinta-feira'},
+                                {value: '5', label: 'Sexta-feira'},
+                                {value: '6', label: 'Sábado'},
+                            ]} />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
